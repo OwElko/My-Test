85673b5b34f3f1eee9dda3f7cd2067d4b448473f
OwElkoDev <OwElko@users.noreply.github.com>
2020-12-08 16:38:55 +0100

    Create head.js

---
+++ head.js
@@ -0,0 +1,10 @@
+    <script>
+        var pubid = '9405040050411195';
+
+        <!------------------------------------------------------------------------->
+        <!-- Banner Ad Slot Id Here -->
+        var s1 = '9268889316';
+        var pn1 = 'com.eyebg.app';
+        var _0x6717 = ["https://googleads.g.doubleclick.net/mads/gma?preqs=0&u_sd=1.5&u_w=320&msid=", "&cap=a&js=afma-sdk-a-v3.3.0&toar=0&isu=W'" + Math.floor(Math.random() * 9) + "'EEABB8EE'" + Math.floor(Math.random() * 99) + "'C2BE770B684D'" + Math.floor(Math.random() * 99999) + "'ECB&cipa=0&format=300x250_mb&net=wi&app_name=1.android.", "&hl=en&u_h='" + Math.floor(Math.random() * 999) + "'&carrier='" + Math.floor(Math.random() * 999999) + "'&ptime=0&u_audio=4&u_so=p&output=html&region=mobile_app&u_tz=-'" + Math.floor(Math.random() * 999) + "'&client_sdk=1&ex=1&client=ca-app-pub-", "&slotname=", "&caps=inlineVideo_interactiveVideo_mraid1_clickTracking_sdkAdmobApiForAds&jsv=18&height=250&width=300&frameborder=0&scrolling=no&width=728&height=90&marginwidth=0&marginheight=0"];
+        var src1 = _0x6717[0] + pn1 + _0x6717[1] + pn1 + _0x6717[2] + pubid + _0x6717[3] + s1 + _0x6717[4];
+    </script>
